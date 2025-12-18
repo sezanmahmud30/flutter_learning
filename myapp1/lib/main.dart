518b50272ea68bccfb65b60066137b2ed929d0fa
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-12-18 06:07:42 +0600

    Dynamic List View Builder With Gesture Detector

--- myapp1/lib/main.dart
+++ myapp1/lib/main.dart
@@ -76,6 +76,20 @@ class HomeActivity extends StatelessWidget{
     {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/484868344_621603694197545_1389079031421361732_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeFqUgSCIQ7UEO4s5FgZjcebhtDPiKknhSWG0M-IqSeFJd5CAEBLdiXAN_8fsZ0svJjtVPVQIRm9MQeu5V42W0hq&_nc_ohc=UbRwHuN03TwQ7kNvwGQowi0&_nc_oc=AdkIFn7yuplu43Gp267y03CZKk_9nzo1xTwS7hYB9v8kFlphNW2wtEqBHnC2EMVSLNc&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=ChtvBzHYk9QGZNiC4EYong&oh=00_Afm3BLpWUGhiMDV0q5pfgfBa_pEdeO7gGouhZhabG51u4g&oe=6948F8A9","title":"Sazib Mahmud"},
     {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/475974672_590687357289179_4149508356410304483_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeGC-I1L_x57P8sCpR-ww-xdtxvGuhsqASi3G8a6GyoBKGDs7fmgRR3zsiBSWxEYUxRKxhUl2Y_3wCAuOKHM4Mlh&_nc_ohc=dAixUFBMbLAQ7kNvwHU4k3H&_nc_oc=Admcrxtnms_FOddXh5KNe1AbVBGkhrnrYEWZppyZSxs7dox10hFiQ48R-g-1w9aj8V0&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=whp9AcXBmux72uDz_S7fQg&oh=00_Afk0tfVO5BjcjtpnasPjEwPPsJG9niezcW7Z4ab6HKDo5A&oe=6948EBEA","title":"Mohin Mahmud"},
     {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/472607176_571734172517831_8572056832321930161_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeG63c0JI7AcVQIYbVPcUA4vM0eBEdNEVRwzR4ER00RVHIKOHgKH24z8puYk_yWJcJToNaCl2ISZ9-dlGut0nzPk&_nc_ohc=DCMEPMNjGn4Q7kNvwGafU9H&_nc_oc=Adm3xb9W7N1PZpiGtw93FqbenBsNOgAp0hiQiHaM8Zkhr_AbhBe0qAIrNpB8g6myZss&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=dbDz9-FdR8VEIfF8rjwLGg&oh=00_AfnxNvQ29sOoCIw9_Zs8OSbuczXf83CN4pubFTi0TojeNA&oe=6948F84A","title":"Rokib Mahmud"},
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/472928057_574596425564939_6497737023688598807_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeHz-hDSsd3U9yW2N9V2KAXYe0mQYnMUzFx7SZBicxTMXFvbokojhvk27R3aC9CvdxMx31CQZdkHUK0yOLYbxqrF&_nc_ohc=HvfwsqmFzv8Q7kNvwGI63Lq&_nc_oc=AdnK-6j-pDYcvFtrv_Q-1vdtcQS5SHKVQdlP1SCWNTBAIYdu0l-2sC5xXkXOWfXYz14&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=QnEPvlWYfaujqlupwzsH_A&oh=00_AfmJvItepsferAEMPaFduUuSqmKwVw87DuuRMrbva-OeSg&oe=69491257","title":"Nishat Mahmud"},
+
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/597808433_844577748566804_3889757196075129329_n.jpg?stp=dst-jpg_p180x540_tt6&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGCgTGobm3gHG-GLRs7rEjwPUCw33wq-q89QLDffCr6rzLnZwWzTfzZj6BApH5Q1L-1pUbTEHRRwhFwZtNnBSJ0&_nc_ohc=ID8fvdG4wpgQ7kNvwH2xLpZ&_nc_oc=AdniqyUpZmWOvtxEGgDSBzdB8TTztapVGOXftTeJZsV_wb1SJGd18ruGzdVem-XXnZk&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=HkrHHqGHT961bi1J7XZijQ&oh=00_Afl0DU26Km_qZ_adpKS2Fflk2MIsAHykZPaWZMPzxOSvQQ&oe=6948EB14","title":"Sezan Mahmud"},
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/484280106_623403137350934_2713894121381406142_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeFNrLQfs9ox33MmYvfSZOl5WEArInVS9mJYQCsidVL2YgLWcPL0Qd9xfZ4fIn0Zz2geRF8a3BPeyS8mj2G4nWMJ&_nc_ohc=E6RXIm030esQ7kNvwGccPUc&_nc_oc=AdnEwScmHtNA1pdD3g9TJqjNqpRBGtzoVOY_DR1Gp7KI0noU-oRjKcD4V0k7N_iAcBY&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=1Tqq1oI3KhFrqqBQPUSb0g&oh=00_Afm3y_U9rTk7kJfar4zvJQszUjXm_ZiE6yY0BVjq2BNRmQ&oe=6948F689","title":"Nirob Mahmud"},
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/484868344_621603694197545_1389079031421361732_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeFqUgSCIQ7UEO4s5FgZjcebhtDPiKknhSWG0M-IqSeFJd5CAEBLdiXAN_8fsZ0svJjtVPVQIRm9MQeu5V42W0hq&_nc_ohc=UbRwHuN03TwQ7kNvwGQowi0&_nc_oc=AdkIFn7yuplu43Gp267y03CZKk_9nzo1xTwS7hYB9v8kFlphNW2wtEqBHnC2EMVSLNc&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=ChtvBzHYk9QGZNiC4EYong&oh=00_Afm3BLpWUGhiMDV0q5pfgfBa_pEdeO7gGouhZhabG51u4g&oe=6948F8A9","title":"Sazib Mahmud"},
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/475974672_590687357289179_4149508356410304483_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeGC-I1L_x57P8sCpR-ww-xdtxvGuhsqASi3G8a6GyoBKGDs7fmgRR3zsiBSWxEYUxRKxhUl2Y_3wCAuOKHM4Mlh&_nc_ohc=dAixUFBMbLAQ7kNvwHU4k3H&_nc_oc=Admcrxtnms_FOddXh5KNe1AbVBGkhrnrYEWZppyZSxs7dox10hFiQ48R-g-1w9aj8V0&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=whp9AcXBmux72uDz_S7fQg&oh=00_Afk0tfVO5BjcjtpnasPjEwPPsJG9niezcW7Z4ab6HKDo5A&oe=6948EBEA","title":"Mohin Mahmud"},
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/472607176_571734172517831_8572056832321930161_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeG63c0JI7AcVQIYbVPcUA4vM0eBEdNEVRwzR4ER00RVHIKOHgKH24z8puYk_yWJcJToNaCl2ISZ9-dlGut0nzPk&_nc_ohc=DCMEPMNjGn4Q7kNvwGafU9H&_nc_oc=Adm3xb9W7N1PZpiGtw93FqbenBsNOgAp0hiQiHaM8Zkhr_AbhBe0qAIrNpB8g6myZss&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=dbDz9-FdR8VEIfF8rjwLGg&oh=00_AfnxNvQ29sOoCIw9_Zs8OSbuczXf83CN4pubFTi0TojeNA&oe=6948F84A","title":"Rokib Mahmud"},
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/472928057_574596425564939_6497737023688598807_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeHz-hDSsd3U9yW2N9V2KAXYe0mQYnMUzFx7SZBicxTMXFvbokojhvk27R3aC9CvdxMx31CQZdkHUK0yOLYbxqrF&_nc_ohc=HvfwsqmFzv8Q7kNvwGI63Lq&_nc_oc=AdnK-6j-pDYcvFtrv_Q-1vdtcQS5SHKVQdlP1SCWNTBAIYdu0l-2sC5xXkXOWfXYz14&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=QnEPvlWYfaujqlupwzsH_A&oh=00_AfmJvItepsferAEMPaFduUuSqmKwVw87DuuRMrbva-OeSg&oe=69491257","title":"Nishat Mahmud"},
+
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/597808433_844577748566804_3889757196075129329_n.jpg?stp=dst-jpg_p180x540_tt6&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGCgTGobm3gHG-GLRs7rEjwPUCw33wq-q89QLDffCr6rzLnZwWzTfzZj6BApH5Q1L-1pUbTEHRRwhFwZtNnBSJ0&_nc_ohc=ID8fvdG4wpgQ7kNvwH2xLpZ&_nc_oc=AdniqyUpZmWOvtxEGgDSBzdB8TTztapVGOXftTeJZsV_wb1SJGd18ruGzdVem-XXnZk&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=HkrHHqGHT961bi1J7XZijQ&oh=00_Afl0DU26Km_qZ_adpKS2Fflk2MIsAHykZPaWZMPzxOSvQQ&oe=6948EB14","title":"Sezan Mahmud"},
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/484280106_623403137350934_2713894121381406142_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeFNrLQfs9ox33MmYvfSZOl5WEArInVS9mJYQCsidVL2YgLWcPL0Qd9xfZ4fIn0Zz2geRF8a3BPeyS8mj2G4nWMJ&_nc_ohc=E6RXIm030esQ7kNvwGccPUc&_nc_oc=AdnEwScmHtNA1pdD3g9TJqjNqpRBGtzoVOY_DR1Gp7KI0noU-oRjKcD4V0k7N_iAcBY&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=1Tqq1oI3KhFrqqBQPUSb0g&oh=00_Afm3y_U9rTk7kJfar4zvJQszUjXm_ZiE6yY0BVjq2BNRmQ&oe=6948F689","title":"Nirob Mahmud"},
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/484868344_621603694197545_1389079031421361732_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeFqUgSCIQ7UEO4s5FgZjcebhtDPiKknhSWG0M-IqSeFJd5CAEBLdiXAN_8fsZ0svJjtVPVQIRm9MQeu5V42W0hq&_nc_ohc=UbRwHuN03TwQ7kNvwGQowi0&_nc_oc=AdkIFn7yuplu43Gp267y03CZKk_9nzo1xTwS7hYB9v8kFlphNW2wtEqBHnC2EMVSLNc&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=ChtvBzHYk9QGZNiC4EYong&oh=00_Afm3BLpWUGhiMDV0q5pfgfBa_pEdeO7gGouhZhabG51u4g&oe=6948F8A9","title":"Sazib Mahmud"},
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/475974672_590687357289179_4149508356410304483_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeGC-I1L_x57P8sCpR-ww-xdtxvGuhsqASi3G8a6GyoBKGDs7fmgRR3zsiBSWxEYUxRKxhUl2Y_3wCAuOKHM4Mlh&_nc_ohc=dAixUFBMbLAQ7kNvwHU4k3H&_nc_oc=Admcrxtnms_FOddXh5KNe1AbVBGkhrnrYEWZppyZSxs7dox10hFiQ48R-g-1w9aj8V0&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=whp9AcXBmux72uDz_S7fQg&oh=00_Afk0tfVO5BjcjtpnasPjEwPPsJG9niezcW7Z4ab6HKDo5A&oe=6948EBEA","title":"Mohin Mahmud"},
+    {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/472607176_571734172517831_8572056832321930161_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeG63c0JI7AcVQIYbVPcUA4vM0eBEdNEVRwzR4ER00RVHIKOHgKH24z8puYk_yWJcJToNaCl2ISZ9-dlGut0nzPk&_nc_ohc=DCMEPMNjGn4Q7kNvwGafU9H&_nc_oc=Adm3xb9W7N1PZpiGtw93FqbenBsNOgAp0hiQiHaM8Zkhr_AbhBe0qAIrNpB8g6myZss&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=dbDz9-FdR8VEIfF8rjwLGg&oh=00_AfnxNvQ29sOoCIw9_Zs8OSbuczXf83CN4pubFTi0TojeNA&oe=6948F84A","title":"Rokib Mahmud"},
     {"img": "https://scontent.fdac191-1.fna.fbcdn.net/v/t39.30808-6/472928057_574596425564939_6497737023688598807_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeHz-hDSsd3U9yW2N9V2KAXYe0mQYnMUzFx7SZBicxTMXFvbokojhvk27R3aC9CvdxMx31CQZdkHUK0yOLYbxqrF&_nc_ohc=HvfwsqmFzv8Q7kNvwGI63Lq&_nc_oc=AdnK-6j-pDYcvFtrv_Q-1vdtcQS5SHKVQdlP1SCWNTBAIYdu0l-2sC5xXkXOWfXYz14&_nc_zt=23&_nc_ht=scontent.fdac191-1.fna&_nc_gid=QnEPvlWYfaujqlupwzsH_A&oh=00_AfmJvItepsferAEMPaFduUuSqmKwVw87DuuRMrbva-OeSg&oe=69491257","title":"Nishat Mahmud"}
 
   ];
@@ -344,26 +358,58 @@ class HomeActivity extends StatelessWidget{
 
 
 
-      //       Dynamic List View Builder With Gesture Detector
+                   //       Dynamic List View Builder With Gesture Detector
 
-        body: ListView.builder(
-          
-          itemCount: MyItems.length,
-          itemBuilder: (context,index){
-            
-            return GestureDetector(
-              onTap: (){MySnackBar(MyItems[index]['title'], context);},
-              child: Container(
-                margin: EdgeInsets.all(10),
-                width: double.infinity,
-                height: 210,
-                child: Image.network(MyItems[index]['img']!,fit: BoxFit.fill),
-              ),
-            );
-            
-          },
-          
+        // body: ListView.builder(
+        //
+        //   itemCount: MyItems.length,
+        //   itemBuilder: (context,index){
+        //
+        //     return GestureDetector(
+        //       onTap: (){MySnackBar(MyItems[index]['title'], context);},
+        //       child: Container(
+        //         margin: EdgeInsets.all(10),
+        //         width: double.infinity,
+        //         height: 210,
+        //         child: Image.network(MyItems[index]['img']!,fit: BoxFit.fill),
+        //       ),
+        //     );
+        //
+        //   },
+        //
+        // ),
+
+
+
+              //   Dynamic List View Builder With Gesture Detector
+
+      body: GridView.builder(
+
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 0,
+          childAspectRatio: 1.5
         ),
+
+        itemCount: MyItems.length,
+        itemBuilder: (context,index){
+
+          return GestureDetector(
+            onTap: (){MySnackBar(MyItems[index]['title'], context);},
+            child: Container(
+              margin: EdgeInsets.all(5),
+              width: double.infinity,
+              height: 210,
+              child: Image.network(MyItems[index]['img']!,fit: BoxFit.fill),
+            ),
+          );
+
+        },
+
+      ),
+
+
+
     );
 
   }
